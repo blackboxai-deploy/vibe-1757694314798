58cfe01e374fc804060d34e61d19b876e8181d98
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 21:56:21 +0530

    Add analytics/models.py

---
+++ analytics/models.py
@@ -0,0 +1,7 @@
+"""
+Models for analytics app - mainly used for reporting and data aggregation
+"""
+from django.db import models
+
+# This app mainly uses models from other apps for reporting
+# No specific models needed here for now
