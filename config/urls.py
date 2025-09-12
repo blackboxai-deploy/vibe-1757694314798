eb17a201ab8ee8757902c0a9b4086809bbf6fb02
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 21:55:52 +0530

    Add config/urls.py

---
+++ config/urls.py
@@ -0,0 +1,33 @@
+"""
+Stock Management System URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('inventory/', include('inventory.urls')),
+    path('analytics/', include('analytics.urls')),
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
+]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+    # Debug toolbar URLs
+    if 'debug_toolbar' in settings.INSTALLED_APPS:
+        import debug_toolbar
+        urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+
+# Customize admin site
+admin.site.site_header = "Sistema de Gerenciamento de Estoque"
+admin.site.site_title = "SGE Admin"
+admin.site.index_title = "Painel de Administração"
